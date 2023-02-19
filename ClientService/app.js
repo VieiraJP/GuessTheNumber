0d12472185b5544c955b9fa27e43fd4c602da9c0
VieiraJP <braulio.vieira@laerdal.com>
2023-02-19 12:57:57 +0100

    Add routes to the client service, some clean up

--- ClientService/app.js
+++ ClientService/app.js
@@ -1,83 +1,13 @@
-require('dotenv').config();
+require('dotenv').config(); // require .env file for environment variables
+const express = require('express'); //require express
+const player = require('./routes/player'); //import routes
+const PORT = process.env.PORT; //Get the port from the environment variable
+const app = express(); //Initiate application
 
-const express = require('express');
-const axios = require('axios');
-
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-// API endpoint to initiate a new game
-const INIT_GAME_URL = 'http://localhost:3000/init';
-
-const isGameRunning=false;
-let gameId=null;
-
-async function binarySearchGame(headers,gameId, low, high) {
-  const mid = Math.floor((low + high) / 2);
-  const guess = { number: mid };
-
-  // Send a guess to the game host service
-  const response = await axios.post(`http://localhost:3000/guess/${gameId}`, {"guess":guess},{headers});
-  const result = response.data;
-
-  if (result === 'correct') {
-    console.log(`Found the number! It's ${mid}`);
-    return mid;
-  } else if (result === 'smaller') {
-    console.log(`Guessing lower...`);
-    return await binarySearchGame(headers,gameId, mid + 1, high);
-
-  } else {
-    console.log(`Guessing higher...`);
-    return await binarySearchGame(headers,gameId, low, mid - 1);
-
-  }
-}
-
-// Route to start a new game and perform the binary search
-app.get('/gameBinarySearch', async (req, res) => {
-  try {
-    // Initiate a new game
-
-    const headers = { Authorization: req.headers.authorization };
-    const response = await axios.post(INIT_GAME_URL, null, {headers});
-    const gameId = response.data.id;
-
-    // Start the binary search
-    const result = await binarySearchGame(headers,gameId, 1, 10000);
-    res.json({ result });
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).json({ message: 'Internal Server Error' });
-  }
-});
-
-// Route to start a new game without binary search
-app.get('/game/:number', async (req, res) => {
-  try {
-    // Initiate a new game
-
-    const headers = { Authorization: req.headers.authorization };
-    const guess  = req.params
-
-    if(gameId==null) {
-      const gameIdResponse = await axios.post(INIT_GAME_URL, null, {headers});
-      gameId = gameIdResponse.data.id;
-    }
-    // Send a guess to the game host service
-    const guessResponse = await axios.post(`http://localhost:3000/guess/${gameId}`, {"guess":guess},{headers});
-    res.json(guessResponse.data);
-
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).json({ message: 'Internal Server Error' });
-  }
-});
-
+app.use(player)//Add the player router to the application
 
 app.listen(PORT, () => {
   console.log(`Player service running on port ${PORT}`);
 });
 
-//TODO: urls  to be changed to env variables
-//TODO: add routes folder to simplify the code in app.js
+module.exports = app;
---
+++ ClientService/routes/player.js
@@ -0,0 +1,140 @@
+require('dotenv').config(); // require .env file for environment variables
+const express = require('express'); //require express
+const axios = require('axios'); ////require axios for http requests
+
+
+//This is the routes file for the client service
+const router = express.Router();
+
+//API endpoint to initiate a new game
+const GAME_URL = process.env.GAME_URL;
+
+const CORRECT = 'correct';
+const SMALLER = 'smaller';
+const LARGER = 'larger';
+
+let isGameInitiated=false;
+let currentGameId=0;
+
+async function binarySearchGame(headers,gameId, low, high) {
+    const mid = Math.floor((low + high) / 2);
+    const guessingNumber = { "guess": mid };
+
+    //try catch block to catch errors
+    try {
+        // Send a guess to the game host service
+        const response = await axios.post(GAME_URL + `/guess/${gameId}`, guessingNumber, {headers});
+        const result = response.data;
+
+        if (result === CORRECT) {
+            console.log(`Found the number! It's ${mid}`);
+            return mid;
+        } else if (result === SMALLER) {
+            console.log(`Guessing lower...`);
+            return await binarySearchGame(headers,gameId, mid + 1, high);
+        } else if (result === LARGER){
+            console.log(`Guessing higher...`);
+            return await binarySearchGame(headers,gameId, low, mid - 1);
+
+        }else {
+            return {"Something went wrong": result};
+
+        }
+    }catch (error){
+        console.error(error.status);
+        return {"Something went wrong": error.response.data};
+        }
+
+ //Compare the result with the guessing number
+
+}
+
+router.get('/initWithBinarySearch',async (req, res) => {
+    try {
+        // Initiate a new game
+        const headers = { Authorization: req.headers.authorization };
+        const response = await axios.post(GAME_URL+'/init', null, {headers});
+        const gameId = response.data.id;
+
+        // Start the binary search
+        const result = await binarySearchGame(headers,gameId, 1, 10000);
+        res.json({ result });
+    } catch (error) {
+        console.error(error.status);
+        res.status(error.status).json({ message: error.message});
+    }
+});
+
+
+//Initiate a new game with
+router.get('/init/', async (req, res) => {
+    try {
+        // Initiate a new game
+        const headers = { Authorization: req.headers.authorization };
+        const response = await axios.post(GAME_URL+'/init', null, {headers});
+        currentGameId = response.data.id;
+        if(currentGameId!==0){
+            isGameInitiated=true;
+        }
+        res.json({ "gameId":currentGameId });
+    } catch (error) {
+        console.error(error.status);
+        res.status(error.status).json({ message: error.message});
+    }
+});
+
+router.get('/game/:numberToGuess', async (req, res) => {
+    try {
+        const headers = { Authorization: req.headers.authorization };
+        const numberToGuess  = req.params
+
+        //Verify that the game is initiated
+        if(!isGameInitiated || currentGameId===0){
+            return res.send(`Please initiate a game first`);
+        }
+
+        // Send a guess to the game host service
+        const guessResponse = await axios.post(GAME_URL+`/guess/${currentGameId}`, {"guess":numberToGuess},{headers});
+        const result = guessResponse.data;
+        if (result=== CORRECT) {
+            console.log(`Found the number! You can now delete the game`);
+            isGameInitiated=false;
+            currentGameId=0
+            return res.send(`Found the number!`);
+        } else if (result === SMALLER) {
+            console.log(`Guessing lower...`);
+            return res.send(`Your Number is smaller than the number to guess`);
+        } else if (result=== LARGER){
+            console.log(`Guessing higher...`);
+            return res.send(`Your Number is larger than the number to guess`);
+
+        }else {
+            return {"Something went wrong": result};
+
+        }
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+});
+
+//Player service should be able to delete a game
+router.delete('/delete/:gameId', async (req, res) => {
+    //send a delete request to the game host service
+    try {
+        const headers = { Authorization: req.headers.authorization };
+        const response = await axios.delete(GAME_URL + `/delete/${req.params.gameId}`, {headers});
+        const result = response.data;
+        if (result === "Game deleted") {
+            console.log(`Game deleted`);
+            return res.send(`Game deleted`);
+        } else {
+            return res.send(`Game not deleted`);
+        }
+    }catch (error) {
+        console.error(error.status);
+        return res.status(error.status).json({message: error.message});
+    }
+});
+
+module.exports = router;
